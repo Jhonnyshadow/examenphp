a6df57506ba1f582792c40065012069b754c6f50
Jhonnyshadow <41090671+Jhonnyshadow@users.noreply.github.com>
2018-12-06 13:09:26 -0500

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -28,7 +28,7 @@ switch ($opcion) {
       echo $crudModel->getProductos();
         $_SESSION['listaProductos'] = serialize($listaProductos);
         //redireccionamos a una nueva pagina para visualizar:
-       //header('Location: ../index.php');
+       header('Location: ../index.php');
         break;
     
     case "crear_producto":
